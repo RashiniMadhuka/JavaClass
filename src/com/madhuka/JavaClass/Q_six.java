e48f9d7d581edc44ef14ebc1c35e66ed2f04ca35
Rashini Madhuka <Rashini Madhuka>
2018-03-08 03:37:20 +0530

    Area of a rectangle

---
+++ src/com/madhuka/JavaClass/Q_six.java
@@ -0,0 +1,19 @@
+package com.madhuka.JavaClass;
+
+import java.util.Scanner;
+
+public class Q_six {
+	private static Scanner num;
+
+	public static void main(String[] args) {
+		num = new Scanner(System.in);
+		float length,width;
+		System.out.print("Enter Your Length : ");
+		length = num.nextFloat();
+		System.out.print("Enter Your Width : ");
+		width = num.nextFloat();
+		System.out.println("Area of the rectangle is : " + (length*width));
+
+	}
+
+}
